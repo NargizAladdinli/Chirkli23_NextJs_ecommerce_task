116c58e21e9d5526ceeddd8580fc254d00bd8f79
nargizaladdinli <nargizalieva05@gmail.com>
2023-06-22 11:50:08 +0400

    Sidebar handle click

--- chirkli23/src/components/sidebar/SideBar.components.jsx
+++ chirkli23/src/components/sidebar/SideBar.components.jsx
@@ -6,7 +6,7 @@ const SideBar = () => {
     const {open, handleClose} = useContext(SidebarContext)
     return(
         <div className={ open ? "open" : "closes" }> 
-            <h1 onClick={handleClose}>SSS</h1>
+            
         </div>
     );
 }
